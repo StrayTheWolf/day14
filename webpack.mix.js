24124408f5ec60ba6e3b3123e9d8208bdaa5e73c
Wolf Stanislav <ork2004@yandex.ru>
2021-01-19 14:44:08 +0300

    added processCssUrls : false in webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,8 @@
 let mix = require('laravel-mix');
 
-mix.sass('src/scss/styles.scss', 'dist/css');
+mix.sass('src/scss/styles.scss', 'dist/css').options({processCssUrls : false});
 
 
 mix.copy('src/index.html', 'dist/index.html');
 mix.copyDirectory('src/images', 'dist/images');
+
